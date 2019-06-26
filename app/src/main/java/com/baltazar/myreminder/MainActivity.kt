bcfad0f53ad58705e78c787c9ba859dfb76f64f7
Baltazar Rodriguez Ramirez <baltazar@MacBook-Pro-de-Baltazar.local>
2019-06-25 22:24:08 -0500

    Add binding with UI elements in mainactivity

--- app/src/main/java/com/baltazar/myreminder/MainActivity.kt
+++ app/src/main/java/com/baltazar/myreminder/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.baltazar.myreminder
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
+    private var mButtonAddRemainder: Button? = null
+    private var mButtonOpenRemainders: Button? = null
+    private var mTextGreetings: TextView? = null
+    private var mToolbar: Toolbar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupWidget()
+    }
+
+    private fun setupWidget() {
+        mButtonAddRemainder = findViewById(R.id.button_add_remainder)
+        mButtonOpenRemainders = findViewById(R.id.button_open_remainders)
+        mTextGreetings = findViewById(R.id.text_greetings)
+        mToolbar = findViewById(R.id.toolbar)
+
+        mToolbar?.let {
+            it.setTitleTextColor(Color.WHITE)
+            setSupportActionBar(it)
+            supportActionBar?.setTitle(resources.getString(R.string.app_name))
+        }
     }
 }
